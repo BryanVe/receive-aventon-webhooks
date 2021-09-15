3e5d2211f6ce06a1c11851f54bf84c0ad0d93f78
Bryan Vera <bryan@chazki.com>
2021-09-15 15:35:50 -0500

    update port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
